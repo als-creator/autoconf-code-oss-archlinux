1120e47b9d296c7f4620d4a75e97e0e3c6f0ed1c
Aleksandr <mrstuzer@gmail.com>
2025-10-03 17:33:06 +0600

    Update autoconf-code-oss.sh
    Убрал команды установки расширений, https://open-vsx.org/ не умеет ставить расширения через консоль

--- autoconf-code-oss.sh
+++ autoconf-code-oss.sh
@@ -10,23 +10,15 @@ sudo pacman -S code git base-devel npm nodejs yarn jq zip unzip wget curl python
 USERNAME="$(whoami)" # Текущий пользователь
 CONFIG_DIR="/home/$USERNAME/.config/Code - OSS/User"
 SETTINGS_FILE="${CONFIG_DIR}/settings.json"
-EXTENSIONS_DIR="/home/$USERNAME/.vscode-oss/extensions"
 
 # Создаем папку User, если её нет
 mkdir -p "$CONFIG_DIR"
 
 # Скачиваем настройки и список расширений с GitHub
 SETTINGS_JSON_URL="https://raw.githubusercontent.com/als-creator/autoconf-code-oss/main/settings.json"
-EXTENSIONS_URL="https://raw.githubusercontent.com/als-creator/autoconf-code-oss/main/extensions.txt"
 
 wget -qO- "$SETTINGS_JSON_URL" > "$SETTINGS_FILE"
 
-# Устанавливаем расширения
-EXTENSIONS=$(curl -s "$EXTENSIONS_URL")
-for ext in ${EXTENSIONS}; do
-  code --install-extension "$ext"
-done
-
 # Готово!
-echo "Все настройки и расширения установлены!"
+echo "Все настройки установлены!"
 exit 0
